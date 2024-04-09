07afe83d410b2ac9791f83d34cf377e1fa15ec06
Jhonsking49 <juanreygonzalez2002@gmail.com>
2024-04-09 02:11:38 +0200

    09-4-24

---
+++ React/JuanRey/Ejercicio 2/src/components/Lista.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import { getVehiculos } from '../helper/getVehiculos';
+
+function Lista() {
+    const [vehiculos, setVehiculos] = useState([]);
+
+    useEffect(() => {
+    
+        getVehiculos((data) => {
+        setVehiculos(data);
+        });
+    }, [vehiculos]);
+
+    const getCardStyle = (tipo) => {
+        switch (tipo) {
+        case 'Coche':
+            return { backgroundColor: 'blue' };
+        case 'Moto':
+            return { backgroundColor: 'grey' };
+        case 'Camion':
+            return { backgroundColor: '#654321' };
+        default:
+            return {};
+        }
+    };
+
+    return (
+        <div className="vehiculos-container">
+        {vehiculos.map((vehiculo, index) => (
+            <div className="vehiculo-card" key={index} style={getCardStyle(vehiculo.tipo)}>
+            <p>Número de Bastidor: {vehiculo.bastidor}</p>
+            <p>Tipo de Vehículo: {vehiculo.tipo}</p>
+            <p>Marca: {vehiculo.marca}</p>
+            <p>Modelo: {vehiculo.modelo}</p>
+            <p>Color: {vehiculo.color}</p>
+            <p>Matricula: {vehiculo.matricula}</p>
+            </div>
+        ))}
+        </div>
+    );
+}
+
+export default Lista;
