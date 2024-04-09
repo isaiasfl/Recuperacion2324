d935aaae2d30ba525be529a23c2b59ef25b8492e
Jhonsking49 <juanreygonzalez2002@gmail.com>
2024-04-09 12:00:20 +0200

    09-2-24

--- React/JuanRey/Ejercicio 2/src/components/Lista.jsx
+++ React/JuanRey/Ejercicio 2/src/components/Lista.jsx
@@ -1,15 +1,15 @@
 import { useEffect, useState } from 'react';
-import { getVehiculos } from '../helper/getVehiculos';
 
 function Lista() {
     const [vehiculos, setVehiculos] = useState([]);
 
     useEffect(() => {
-    
-        getVehiculos((data) => {
-        setVehiculos(data);
-        });
-    }, [vehiculos]);
+        // Cargar los datos del archivo JSON cuando el componente se monte
+        fetch("/React/JuanRey/Ejercicio 2/server/dgt.json")
+          .then(response => response.json())
+          .then(data => setVehiculos(data.altas))
+          .catch(error => console.error('Error al cargar los vehículos:', error));
+      }, []);
 
     const getCardStyle = (tipo) => {
         switch (tipo) {
