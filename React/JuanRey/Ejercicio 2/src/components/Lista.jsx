3686ddc19f2a42fd3014b81711830a8d995c250b
Jhonsking49 <juanreygonzalez2002@gmail.com>
2024-04-09 12:04:02 +0200

    09-2-24 3º edicion

--- React/JuanRey/Ejercicio 2/src/components/Lista.jsx
+++ React/JuanRey/Ejercicio 2/src/components/Lista.jsx
@@ -4,38 +4,37 @@ function Lista() {
     const [vehiculos, setVehiculos] = useState([]);
 
     useEffect(() => {
-        // Cargar los datos del archivo JSON cuando el componente se monte
         fetch("/React/JuanRey/Ejercicio 2/server/dgt.json")
-          .then(response => response.json())
-          .then(data => setVehiculos(data.altas))
-          .catch(error => console.error('Error al cargar los vehículos:', error));
-      }, []);
+            .then(response => response.json())
+            .then(data => setVehiculos(data.altas))
+            .catch(error => console.error('Error al cargar los vehículos:', error));
+    }, []);
 
     const getCardStyle = (tipo) => {
         switch (tipo) {
-        case 'Coche':
-            return { backgroundColor: 'blue' };
-        case 'Moto':
-            return { backgroundColor: 'grey' };
-        case 'Camion':
-            return { backgroundColor: '#654321' };
-        default:
-            return {};
+            case 'Coche':
+                return 'bg-blue-500';
+            case 'Moto':
+                return 'bg-gray-500';
+            case 'Camion':
+                return 'bg-brown-600';
+            default:
+                return '';
         }
     };
 
     return (
-        <div className="vehiculos-container">
-        {vehiculos.map((vehiculo, index) => (
-            <div className="vehiculo-card" key={index} style={getCardStyle(vehiculo.tipo)}>
-            <p>Número de Bastidor: {vehiculo.bastidor}</p>
-            <p>Tipo de Vehículo: {vehiculo.tipo}</p>
-            <p>Marca: {vehiculo.marca}</p>
-            <p>Modelo: {vehiculo.modelo}</p>
-            <p>Color: {vehiculo.color}</p>
-            <p>Matricula: {vehiculo.matricula}</p>
-            </div>
-        ))}
+        <div className="flex flex-wrap justify-center gap-4">
+            {vehiculos.map((vehiculo, index) => (
+                <div key={index} className={`max-w-md p-4 rounded-lg shadow-md ${getCardStyle(vehiculo.tipo)}`}>
+                    <p className="mb-2">Número de Bastidor: {vehiculo.bastidor}</p>
+                    <p className="mb-2">Tipo de Vehículo: {vehiculo.tipo}</p>
+                    <p className="mb-2">Marca: {vehiculo.marca}</p>
+                    <p className="mb-2">Modelo: {vehiculo.modelo}</p>
+                    <p className="mb-2">Color: {vehiculo.color}</p>
+                    <p className="mb-2">Matricula: {vehiculo.matricula}</p>
+                </div>
+            ))}
         </div>
     );
 }
