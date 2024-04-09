7dfb9f701a32b07ebab8208edf7a7e8e3a541640
Jhonsking49 <juanreygonzalez2002@gmail.com>
2024-04-09 12:12:54 +0200

    09-2-24 4º edicion

--- React/JuanRey/Ejercicio 2/src/App.jsx
+++ React/JuanRey/Ejercicio 2/src/App.jsx
@@ -1,16 +1,22 @@
-
-import Formulario from './components/Formulario'
-import Lista from "./components/Lista"
+import Formulario from './components/Formulario';
+import Lista from "./components/Lista";
 
 function App() {
   return (
-    <>
-      <div className="App">
-        <Formulario />
-        <Lista />
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-2xl font-bold mb-6">Dirección General de Tráfico</h1>
+      <div className="max-w-screen-lg w-full p-6 bg-white rounded-lg shadow-md grid grid-cols-2 gap-6">
+        <div className="col-span-1">
+          <Formulario />
+        </div>
+        <div className="col-span-1">
+          <h2 className="text-xl font-semibold mb-4">Lista de Vehículos</h2>
+          <Lista />
+        </div>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
+
--- React/JuanRey/Ejercicio 2/src/components/Lista.jsx
+++ React/JuanRey/Ejercicio 2/src/components/Lista.jsx
@@ -27,7 +27,7 @@ function Lista() {
         <div className="flex flex-wrap justify-center gap-4">
             {vehiculos.map((vehiculo, index) => (
                 <div key={index} className={`max-w-md p-4 rounded-lg shadow-md ${getCardStyle(vehiculo.tipo)}`}>
-                    <p className="mb-2">Número de Bastidor: {vehiculo.bastidor}</p>
+                    <p className="mb-2">Número de Bastidor: {vehiculo.nbastidor}</p>
                     <p className="mb-2">Tipo de Vehículo: {vehiculo.tipo}</p>
                     <p className="mb-2">Marca: {vehiculo.marca}</p>
                     <p className="mb-2">Modelo: {vehiculo.modelo}</p>
