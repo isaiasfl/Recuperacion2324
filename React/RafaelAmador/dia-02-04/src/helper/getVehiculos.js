d665de6b494e6ce8ac977ba81c78d283af1b113a
RafaelAmador16 <amadorcastellanorafaa@gmail.com>
2024-04-09 19:04:48 +0200

    Borrar codigo sobrante

--- React/RafaelAmador/dia-02-04/src/helper/getVehiculos.js
+++ React/RafaelAmador/dia-02-04/src/helper/getVehiculos.js
@@ -15,36 +15,3 @@ export async function getVehiculos(callback, tipoVehiculo){
         console.error(error);
     }
 }
-
-
-export async function getCoches(callback) {
-    try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        const coches = data.filter(vehiculo => vehiculo.tipo === "Coche");
-        callback(coches);
-    } catch (error) {
-        console.error(error);
-    }
-}
-export async function getMotos(callback) {
-    try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        const coches = data.filter(vehiculo => vehiculo.tipo === "Moto");
-        callback(coches);
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-export async function getCamion(callback) {
-    try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        const coches = data.filter(vehiculo => vehiculo.tipo === "Camion");
-        callback(coches);
-    } catch (error) {
-        console.error(error);
-    }
-}
