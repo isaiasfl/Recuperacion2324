dd0cd489dba4ed6e3272146d5b1b9f03ede0dabd
RafaelAmador16 <amadorcastellanorafaa@gmail.com>
2024-04-10 17:46:44 +0200

    solucion de errores

--- React/RafaelAmador/dia-02-04/src/pages/MainPage.jsx
+++ React/RafaelAmador/dia-02-04/src/pages/MainPage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import Form from "../components/Form"
 import Header from "../components/Header"
 import ListaVehiculos from "../components/ListaVehiculos"
-import { getCamion, getCoches, getMotos, getVehiculos } from "../helper/getVehiculos";
+import {  getVehiculos } from "../helper/getVehiculos";
 
 const MainPage = () => {
   const [vehiculos, setVehiculos] = useState([]);
