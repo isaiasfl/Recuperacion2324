d7f5412742a2cc3f8495214503c0c4ab3d571b2b
jairocomino <jairocominoruiz@gmail.com>
2024-04-30 14:41:24 +0200

    RUTAS V1 y V2 2

--- React/JairoComino/Rutasv2/src/pages/InicioPage.jsx
+++ React/JairoComino/Rutasv2/src/pages/InicioPage.jsx
@@ -4,8 +4,8 @@ import { Link} from 'react-router-dom';
 const InicioPage = (props) => {
 
   const handleSalir=()=>{
-    if (localStorage.getItem('Token')) {
-       localStorage.removeItem('Token');
+    if (localStorage.getItem('Token2')) {
+       localStorage.removeItem('Token2');
        props.setToken(false);
     }
    
